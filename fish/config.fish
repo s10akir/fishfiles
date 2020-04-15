5f30bedf1e2854524b35fe6e011964de04464801
Akira Shinohara <k017c1067@it-neec.jp>
2020-04-15 11:08:21 +0900

    [feat] add rbenv setting

--- fish/config.fish
+++ fish/config.fish
@@ -2,5 +2,13 @@
 set -x GOPATH ~/.go
 set -x PATH $PATH $GOPATH/bin
 
+# rbenv
+set -x RBENV_ROOT /usr/local/rbenv
+set -x PATH $PATH $RBENV_ROOT/bin
+
+if type "rbenv" > /dev/null 2>&1
+  status --is-interactive; and source (rbenv init -|psub)
+end
+
 # starship
 starship init fish | source
